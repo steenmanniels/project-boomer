6bd548fdaa576fac685261723a999f29804160a9
steenmanniels <ncsteenman@gmail.com>
2016-09-28 21:39:27 +0200

    Update gulpfile.js second time

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var elixir = require('laravel-elixir');
  | for your Laravel application. By default, we are compiling the Sass
  | file for our application, as well as publishing vendor resources.
  |  Hallo
- Doeg
  */
 
 elixir(function(mix) {
